a6c80291457885cbfdc1670de26b388c579aec3c
justin.lee2369@gmail.com <justin.lee2369@gmail.com>
2016-05-19 11:17:07 -0700

    loading spin thingy

--- public/js/index.js
+++ public/js/index.js
@@ -21,6 +21,30 @@ function getPoints() {
     return latlangLights;
 }
 
+var opts = {
+  lines: 13 // The number of lines to draw
+, length: 37 // The length of each line
+, width: 12 // The line thickness
+, radius: 41 // The radius of the inner circle
+, scale: 0.5 // Scales overall size of the spinner
+, corners: 1 // Corner roundness (0..1)
+, color: '#000' // #rgb or #rrggbb or array of colors
+, opacity: 0.25 // Opacity of the lines
+, rotate: 0 // The rotation offset
+, direction: 1 // 1: clockwise, -1: counterclockwise
+, speed: 1.1 // Rounds per second
+, trail: 60 // Afterglow percentage
+, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+, zIndex: 2e9 // The z-index (defaults to 2000000000)
+, className: 'spinner' // The CSS class to assign to the spinner
+, top: '50%' // Top position relative to parent
+, left: '50%' // Left position relative to parent
+, shadow: true // Whether to render a shadow
+, hwaccel: false // Whether to use hardware acceleration
+, position: 'absolute' // Element positioning
+}
+
+
 function changeGradient() {
     var gradient = [
         'rgba(0, 255, 255, 0)',
@@ -42,6 +66,11 @@ function changeGradient() {
 }
 
 $(document).ready(function() {
+
+    var target = document.getElementById('spinner')
+    var spinner = new Spinner().spin()
+    target.appendChild(spinner.el)
+
     //selectUber('UberX');
     $.get("/currentCrimes", {
         lat: 32.7157,
@@ -85,6 +114,7 @@ $(document).ready(function() {
 
     setTimeout(function() {
         CreateDirections(start, end, "walking", function(res, err) {
+            spinner.stop();
             console.log(res)
             var info = $("#routeInfo");
             for (var i in res) {
