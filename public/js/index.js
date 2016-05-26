4ebf1ed2d42205eb033cdf10cc384c3ab1193ea9
justin.lee2369@gmail.com <justin.lee2369@gmail.com>
2016-05-25 21:59:15 -0700

    changed map color back to black

--- public/js/index.js
+++ public/js/index.js
@@ -206,116 +206,28 @@ window.initMap = function() {
     });
 
     var styleArray = [{
-        "featureType": "all",
-        "elementType": "labels.text.fill",
-        "stylers": [{
-            "color": "#ffffff"
-        }, {
-            "weight": "0.20"
-        }, {
-            "lightness": "28"
-        }, {
-            "saturation": "23"
-        }, {
-            "visibility": "off"
-        }]
-    }, {
-        "featureType": "all",
-        "elementType": "labels.text.stroke",
-        "stylers": [{
-            "color": "#494949"
-        }, {
-            "lightness": 13
-        }, {
-            "visibility": "off"
-        }]
-    }, {
-        "featureType": "all",
-        "elementType": "labels.icon",
-        "stylers": [{
-            "visibility": "off"
-        }]
-    }, {
-        "featureType": "administrative",
-        "elementType": "geometry.fill",
-        "stylers": [{
-            "color": "#000000"
-        }]
-    }, {
-        "featureType": "administrative",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-            "color": "#144b53"
-        }, {
-            "lightness": 14
-        }, {
-            "weight": 1.4
-        }]
-    }, {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [{
-            "color": "#08304b"
-        }]
-    }, {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#0c4152"
-        }, {
-            "lightness": 5
-        }]
-    }, {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [{
-            "color": "#000000"
-        }]
-    }, {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-            "color": "#0b434f"
-        }, {
-            "lightness": 25
-        }]
-    }, {
-        "featureType": "road.arterial",
-        "elementType": "geometry.fill",
-        "stylers": [{
-            "color": "#000000"
-        }]
-    }, {
-        "featureType": "road.arterial",
-        "elementType": "geometry.stroke",
-        "stylers": [{
-            "color": "#0b3d51"
-        }, {
-            "lightness": 16
-        }]
-    }, {
-        "featureType": "road.local",
-        "elementType": "geometry",
-        "stylers": [{
-            "color": "#000000"
-        }]
-    }, {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [{
-            "color": "#146474"
-        }]
-    }, {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [{
-            "color": "#021019"
-        }]
-    }]
+     "stylers": [{
+         "hue": "#ff1a00"
+     }, {
+         "invert_lightness": true
+     }, {
+         "saturation": -100
+     }, {
+         "lightness": 33
+     }, {
+         "gamma": 0.5
+     }]
+ }, {
+     "featureType": "water",
+     "elementType": "geometry",
+     "stylers": [{
+         "color": "#2D333C"
+     }]
+ }]
 
-    map.setOptions({
-        styles: styleArray
-    });
+ map.setOptions({
+     styles: styleArray
+ });
 
 
     function ToggleControl(controlDiv, map) {
