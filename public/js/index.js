aecd7db0ddbf5a28e81abdc87b146d0ec783fb0a
leonlxli <Li.Lxiao@yahoo.com>
2016-05-26 00:22:15 -0700

    changing light metrics

--- public/js/index.js
+++ public/js/index.js
@@ -142,7 +142,7 @@ function setLightAndCrimeData() {
         var numLights = getNumLights(routeInfo[i].polyline)
         console.log((numLights * 25))
         console.log(routeInfo[i].route.legs[0].distance.value)
-        var lightPercent = ((numLights * 25) / routeInfo[i].route.legs[0].distance.value) * 100
+        var lightPercent = ((numLights * 15) / routeInfo[i].route.legs[0].distance.value) * 100
         var lightText = (Math.round(lightPercent * 100) / 100) + "% lit"
         routeInfo[i].lights = lightPercent;
         routeInfo[i].lightPercentText = lightText
