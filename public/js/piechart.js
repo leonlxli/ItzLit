3840821a6f0d38d220eadcc21ce4b70b7d86f3e7
Alexandra Huang <alexandra.huang@gmail.com>
2016-06-06 18:31:52 -0700

    Added pie chart with working tooltips, fixed formatting of tooltips in line chart

--- public/js/piechart.js
+++ public/js/piechart.js
@@ -1,171 +1,183 @@
     (function($) {
-        "use strict";
-
-        var data = [];
+      "use strict";
+      
+        var data=[];
         var pie_data = [];
 
-        function getCount(data) {
-            return new Promise(function(resolve, reject) {
-                $.get("/getCountCrimeData", function(response) {
-                    data.push(response);
-                    resolve();
-                });
+        function getCount(data){
+          return new Promise(function(resolve, reject){
+            $.get("/getCountCrimeData", function(response){
+                  data.push(response);
+                  resolve();
             });
+          });
         }
 
 
 
         var promises = [];
-        promises.push(getCount(data));
-
+          promises.push(getCount(data));
+        
 
         var total = 0;
 
-        Promise.all(promises).then(function() {
+        Promise.all(promises).then(function(){
+          console.log("dataData", data);
+            console.log("PieData", pie_data);
 
-            for (var a = 0; a < data[0].length; a++) {
-                total = total + parseInt(data[0][a].count); // simple logic to calculate total of data count value
-            }
 
-            for (var a = 0; a < data[0].length; a++) { // simple logic to calculate percentage data for the pie
-                pie_data[a] = {
-                    "count": (data[0][a].count / total) * 100,
-                    "label": data[0][a].label
-                };
+        var origCount = [];
 
-                // pie_data[a] = data[0][a].count;
-            }
-            var width = 360;
-            var height = 360;
-            var radius = Math.min(width, height) / 2;
+        for(var a=0;a<data[0].length;a++){
+          console.log("datao-o", data);
+          console.log("datacount", data[0][a].count);
+          origCount.push(data[0][a].count);
+          console.log("origCount", origCount);
+          total=total+parseInt(data[0][a].count); // simple logic to calculate total of data count value
+          console.log(total);
+        }
+
+        for( var a=0;a<data[0].length;a++){ // simple logic to calculate percentage data for the pie
+          pie_data[a]={"count": (data[0][a].count/total)*100, "label": data[0][a].label};
+
+          // pie_data[a] = data[0][a].count;
+          console.log("each pie",pie_data[a]);
+        }
+              var margin = {top: 20, right: 40, bottom: 120, left: 80},
+      width = 600 - margin.right - margin.left,
+    height = 600 - margin.top - margin.bottom;
+              var radius = Math.min(width, height) / 2;
+              var donutWidth = 95; 
 
             // var color = d3.scale.ordinal()
             //     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-            var color = d3.scale.category10();
+var color = d3.scale.category10();
 
-            var svg = d3.select('#pieChart')
-                .append('svg')
-                .attr('width', width)
-                .attr('height', height)
-                .append('g')
-                .attr('transform', 'translate(' + (width / 2) +
-                    ',' + (height / 2) + ')');
+        var svg = d3.select('#pieChart')
+            .append("text")
+            .attr("class", "title")
+            .attr("x", 0)             
+            .attr("y", -250)
+            .attr("text-anchor", "middle")  
+            .style("font-size", "30px") 
+            .style("fill", "white")
+            // .style("text-decoration", "underline")  
+            .text("Breakdown of Crimes")
+          .append('svg')
+          .attr('width', width + 20)
+          .attr('height', height + 60)
+          .style('text-align', "center")
+          // .style('padding', 20)
+          // .style('padding-right', 60)
+          .append('g')
+          .attr('transform', 'translate(' + (width / 1.9) +
+            ',' + (height / 1.8) + ')');
 
-            // var pie = d3.layout.pie()
-            //   .value(function(d) { return d.count; })
-            //   .sort(null);
+          
 
-            var pie = d3.layout.pie()
-                .value(function(d, i) {
-                    return pie_data[i].count;
-                })
-                .sort(null);
+        // var pie = d3.layout.pie()
+        //   .value(function(d) { return d.count; })
+        //   .sort(null);
 
-            // var pie = d3.layout.pie()
-            //   .value(function(d,i) { return pie_data[i]; })
-            //   .sort(null);
+        var pie = d3.layout.pie()
+          .value(function(d,i) { return pie_data[i].count; })
+          .sort(null);
 
-            var arc = d3.svg.arc()
-                .outerRadius(radius);
-            // .innerRadius(radius - 50);
+        // var div = d3.select("body").append("div")
+        //     .attr("class", "tooltip")
+        //     .style("opacity", 0);
 
-            //  var path = svg.selectAll('path')
-            // .data(pie(pie_data))
-            // .enter()
-            // .append('path')
-            // .attr('d', arc)
-            // .attr('fill', function(d, i) { 
-            //   return color(i);
-            // })
+        var tooltip = d3.select("body")
+          .append("div")                                                
+          .attr("class", "tooltip")          
+          .style("opacity", 0);                         
+                    
+        tooltip.append("div")
+          .attr("class", "label")    
+          .style("opacity", 0);                                     
 
-            var key = function(d) {
-                return d.data.label;
-            };
+        tooltip.append("div")
+          .attr("class", "count")
+          .style("opacity", 0);   
 
-            var path = svg.selectAll('path')
-                .data(pie(pie_data))
-                .enter()
-                .append('path')
-                .attr('d', arc)
-                .attr('fill', function(d, i) {
-                    return color(d.data.label);
-                })
-                .append("g")
-                .attr("class", "labels")
-                .append("g")
-                .attr("class", "lines");
-            //   .append("text")
-            //   .attr("transform", function(d,i) { 
-            //     console.log("dataeee", d.data.count);
-            //     return "translate(" + arc.centroid(d.data.count) + ")"; })
-            // .attr("dy", ".35em")
-            // .attr("text-anchor", "middle")                          //center the text on its origin
-            // // .style("stroke", "white")
-            // // .style("fill", "white")
-            // .text(function(d, i) { return pie_data[i].label; });  
-
-            var text = svg.select(".labels").selectAll("text")
-                .data(pie(pie_data), key);
-
-            text.enter()
-                .append("text")
-                .attr("dy", ".35em")
-                .text(function(d) {
-                    return d.data.label;
-                });
-
-            function midAngle(d) {
-                return d.startAngle + (d.endAngle - d.startAngle) / 2;
-            }
+        tooltip.append("div")
+          .attr("class", "percent")
+          .style("opacity", 0);   
 
 
-            // text.transition().duration(1000)
-            //   .attrTween("transform", function(d) {
-            //     this._current = this._current || d;
-            //     var interpolate = d3.interpolate(this._current, d);
-            //     this._current = interpolate(0);
-            //     return function(t) {
-            //       var d2 = interpolate(t);
-            //       var pos = outerArc.centroid(d2);
-            //       pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-            //       return "translate("+ pos +")";
-            //     };
-            //   })
-            //   .styleTween("text-anchor", function(d){
-            //     this._current = this._current || d;
-            //     var interpolate = d3.interpolate(this._current, d);
-            //     this._current = interpolate(0);
-            //     return function(t) {
-            //       var d2 = interpolate(t);
-            //       return midAngle(d2) < Math.PI ? "start":"end";
-            //     };
-            //   });
 
-            // text.exit()
-            //   .remove();
 
-            var polyline = svg.select(".lines").selectAll("polyline")
-                .data(pie(data), key);
+        var arc = d3.svg.arc()
+          .outerRadius(radius)
+           .innerRadius(radius - donutWidth);
 
-            polyline.enter()
-                .append("polyline");
+        var arcHover = d3.svg.arc()
+        .innerRadius(radius - donutWidth)
+        .outerRadius(radius + 15); 
 
-            // polyline.transition().duration(1000)
-            //   .attrTween("points", function(d){
-            //     this._current = this._current || d;
-            //     var interpolate = d3.interpolate(this._current, d);
-            //     this._current = interpolate(0);
-            //     return function(t) {
-            //       var d2 = interpolate(t);
-            //       var pos = outerArc.centroid(d2);
-            //       pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-            //       return [arc.centroid(d2), outerArc.centroid(d2), pos];
-            //     };      
-            //   });
 
-            // polyline.exit()
-            //   .remove();
+          //  var path = svg.selectAll('path')
+          // .data(pie(pie_data))
+          // .enter()
+          // .append('path')
+          // .attr('d', arc)
+          // .attr('fill', function(d, i) { 
+          //   return color(i);
+          // })
+
+      //var key = function(d){ return d.data.label; };
+
+      var svgContainer = d3.
+      select('#pieChart')
+
+  var tooltip = svgContainer                    
+          .append('div')                                                
+          .attr('class', 'tooltip')
+          .attr('id', 'pieChartTip');                                                        
+    var tooltipLabel = tooltip.append('div')
+          .attr('class', 'label');                                                
+     var tooltipCount = tooltip.append('div')
+          .attr('class', 'count');       
+
+  var tooltipPercent = tooltip.append('div')
+          .attr('class', 'percent'); 
+
+
+        var path = svg.selectAll('path').
+            data(pie(pie_data))
+            .enter()
+            .append('path')
+            .attr('d', arc)
+          .attr('class', 'path')
+            .attr('fill', function(d,i){
+             return color(d.data.label);})
+             .on('mouseover', function(d,i) {
+                var total = d3.sum(data.map(function(d,i) {
+                    console.log("countWHAT", d[i].count);
+                      return d[i].count;
+                   })); 
+                console.log("counterrrrr", d.data.count);
+                console.log("totallllll", total);
+                // var percent = Math.round(1000 * d.data.count / total) / 10;
+                    tooltipLabel.text(d.data.label);
+                    tooltipCount.text(origCount[i]); 
+                    tooltipPercent.text(d.data.count.toFixed(2) + '%'); 
+                    tooltip.style("opacity", 1); 
+                 d3.select(this).transition()
+                  .duration(500)
+                  .attr("d", arcHover);
+                   var total = d3.sum(data.map(function(d) {
+                      return d.count;
+                   })); 
+                   }).       
+                on('mouseout', function() {
+                 d3.select(this).transition()
+                  .duration(500)
+                  .attr("d", arc);      
+                    // tooltip.style('display', 'none'); 
+                    tooltip.style("opacity", 0);                   
+                  });
 
         })
-    })($);
+}) ($);
